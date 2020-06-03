1644cd790307e028334f44f12794d2ed2879417e
fspaolo <fspaolo@gmail.com>
2020-06-03 15:34:49 -0700

    Add intro shell notebook

---
+++ commands.sh
@@ -0,0 +1,19 @@
+pwd
+ls
+ls -1
+ls -l
+cd ~
+pwd
+ls
+cd -
+cd ..
+pwd
+ls
+cd -
+mkdir test
+echo 'this text is printed on the screen'
+echo 'this text is saved to a file' > file.txt
+echo 'this is another line of text' >> file.txt
+mv file.txt test/myfile.txt
+cat test/myfile.txt
+# rm -r test
