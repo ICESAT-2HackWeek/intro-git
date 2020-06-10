63539d27f7bac1a0787f010bba62dcb902f709d5
fspaolo <fspaolo@gmail.com>
2020-06-09 19:00:47 -0700

    Change order of command examples

--- commands.sh
+++ commands.sh
@@ -11,13 +11,14 @@ cd ..
 pwd
 ls
 cd -
-mkdir test
 echo 'this text is printed on the screen'
 echo 'this text is saved to a file' > file.txt
 echo 'this is another line of text' >> file.txt
+cat file.txt
 cp file.txt file2.txt
+mkdir test
 mv file2.txt test/file3.txt
 cat test/file3.txt
 rm test/*
-rm file.txt
 rmdir test
+rm file.txt
